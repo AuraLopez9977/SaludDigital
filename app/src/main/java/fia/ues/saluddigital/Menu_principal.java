aa61583e2fa6fe04f8b5b4fe9abb66542ae4c24a
AuraLopez9977 <71407799+AuraLopez9977@users.noreply.github.com>
2023-06-11 22:54:37 -0600

    aura

--- app/src/main/java/fia/ues/saluddigital/Menu_principal.java
+++ app/src/main/java/fia/ues/saluddigital/Menu_principal.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import fia.ues.saluddigital.RutinaEjercicios.EjercicioVideo;
 import fia.ues.saluddigital.Usuario.RegistroUsuario;
 
 public class Menu_principal extends AppCompatActivity {
-    Button btn_registrarUsuario;
+    Button btn_registrarUsuario,btn_ejercicios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,14 @@ public class Menu_principal extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registrarUs = new Intent(Menu_principal.this, RegistroUsuario.class);
                 startActivity(registrarUs);
+            }
+        });
+        btn_ejercicios = findViewById(R.id.btn_ejercicio);
+        btn_ejercicios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent video = new Intent(Menu_principal.this, EjercicioVideo.class);
+                startActivity(video);
             }
         });
     }
